d646113c71bd5c9ece5655c6cffcb3213e57289b
yashvanth5 <116138513+yashvanth5@users.noreply.github.com>
2022-12-18 19:26:38 +0530

    css added
    added css to all the things in triangles

--- istriangle.js
+++ istriangle.js
@@ -1,4 +1,4 @@
-const inputs = document.querySelectorAll(".angle-input");
+const inputs = document.querySelectorAll("#angle-input");
 const button = document.querySelector("#is-triangle-btn");
 const outputEl = document.querySelector("#output");
 
@@ -13,10 +13,13 @@ function isTriangle (){
     var sumOfAngle = calculateSumOfAngle(Number(inputs[0].value) ,  Number ( inputs[1].value),  Number(inputs[2].value) );
     if(sumOfAngle === 180){
             outputEl.innerText = " amazing , the angles form a traingle";
+            outputEl.style.color = "green";
+       
         }
 
      else {
         outputEl.innerText = " oh shut , the angles dont form a triangle ";
+        outputEl.style.color = "red";
     }
         }
 
